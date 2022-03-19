5cf6b2f2ebd029d6302f4386ee7b5aa0718fdf6d
Anirudh <anirudh.bhosle2003@gmail.com>
2022-03-19 19:56:44 +0530

    Balanced Parenthesis

--- balanced_paranthesis.cpp
+++ balanced_paranthesis.cpp
@@ -8,43 +8,50 @@ bool isvalid(string s)
     bool ans = true;
     for (int i = 0; i < s.size(); i++)
     {
-        if (s[i] == '{' || s[i] == '[' ||s[i] == '(')
+        if (s[i] == '{' || s[i] == '[' || s[i] == '(')
         {
             st.push(s[i]);
         }
         else if (s[i] == ')')
         {
-            if (st.top() == '(')
+            if (!st.empty() && st.top() == '(')
             {
                 st.pop();
             }
-            else{
+            else
+            {
                 ans = false;
                 break;
             }
         }
-        else if (s[i] == ']')
+        else if (!st.empty() && s[i] == ']')
         {
             if (st.top() == '[')
             {
                 st.pop();
             }
-            else{
+            else
+            {
                 ans = false;
                 break;
             }
         }
         else
         {
-            if (st.top() == '{')
+            if (!st.empty() && st.top() == '{')
             {
                 st.pop();
             }
-            else{
+            else
+            {
                 ans = false;
                 break;
             }
         }
+    }
+    if (!st.empty())
+    {
+        return false;
     }
     return ans;
 }
