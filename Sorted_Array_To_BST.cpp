cb562bdaa928ef915adc439152df72d6cdf56e48
Anirudh <anirudh.bhosle2003@gmail.com>
2022-06-24 20:39:56 +0530

    Build Balanced BST from Sorted Array

---
+++ Sorted_Array_To_BST.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left, *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL, right = NULL;
+    }
+};
+
+Node *sortedArrayToBST(int arr[], int start, int end)
+{
+    if (start > end)
+    {
+        return NULL;
+    }
+    int mid = (start + end) / 2;
+    Node *root = new Node(arr[mid]);
+    root->left = sortedArrayToBST(arr, start, mid - 1);
+    root->right = sortedArrayToBST(arr, mid + 1, end);
+    return root;
+}
+
+void inOrder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    Node *root = sortedArrayToBST(arr, 0, 8);
+    inOrder(root);
+    return 0;
+}
