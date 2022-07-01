235dbb662c0aa1002fd553dbc6adfa23969f4dd5
Anirudh <anirudh.bhosle2003@gmail.com>
2022-07-01 11:05:16 +0530

    Indian Coin Change - Greedy Algorithm

---
+++ Indian_Coin_Change.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int x;
+    cin >> x;
+    int ans = 0;
+    sort(arr.begin(), arr.end(), greater<int>());
+    for (int i = 0; i < n; i++)
+    {
+        ans += x / arr[i];
+        x -= (x / arr[i]) * arr[i];
+    }
+    cout << ans << endl;
+
+    return 0;
+}
